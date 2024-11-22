ec553dfa3dd33cec0fb49b1797691690b329a43e
MoeMahhouk <mohammed-mahhouk@hotmail.com>
2024-11-22 18:07:25 +0000

    feat: Add nacl encryption

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), ProviderError> {
+    info!("Initializing sodiumoxide");
+    crypto::init_sodium()?;
+
     env_logger::init();
     info!("Starting Gramine Sealing Key Provider");
 
